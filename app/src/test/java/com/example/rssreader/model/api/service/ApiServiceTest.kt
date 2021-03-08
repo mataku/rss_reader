02ee0b6440b928a643189d98d3b6e184cd2ef922
Takuma Homma <nagomimatcha@gmail.com>
2021-03-08 23:36:59 +0900

    Add test for GET /hotentry.rss to check whether response is parsed correctly

---
+++ app/src/test/java/com/example/rssreader/model/api/service/ApiServiceTest.kt
@@ -0,0 +1,54 @@
+package com.example.rssreader.model.api.service
+
+import com.example.rssreader.model.api.entity.HatenaRss
+import com.example.rssreader.test_helper.AssetsHelper
+import com.example.rssreader.test_helper.RetrofitFactory
+import kotlinx.coroutines.runBlocking
+import okhttp3.mockwebserver.MockResponse
+import okhttp3.mockwebserver.MockWebServer
+import okhttp3.mockwebserver.RecordedRequest
+import org.junit.runner.RunWith
+import org.robolectric.RobolectricTestRunner
+import kotlin.test.AfterTest
+import kotlin.test.BeforeTest
+import kotlin.test.Test
+import kotlin.test.assertTrue
+
+@RunWith(RobolectricTestRunner::class)
+class ApiServiceTest {
+
+    private val mockWebServer = MockWebServer()
+
+    @BeforeTest
+    fun setUp() {
+        mockWebServer.start()
+    }
+
+    @AfterTest
+    fun tearDown() {
+        mockWebServer.shutdown()
+    }
+
+    @Test
+    fun getOverallEntries() = runBlocking {
+        val dispatcher = object : okhttp3.mockwebserver.Dispatcher() {
+            override fun dispatch(request: RecordedRequest): MockResponse {
+                return if (request.path == "/hotentry.rss") {
+                    val body = AssetsHelper.getAssetFileStrings("hotentry.xml")
+                    println(body)
+                    MockResponse().setResponseCode(200)
+                        .setBody(body)
+                } else {
+                    MockResponse().setResponseCode(404)
+                }
+            }
+        }
+        mockWebServer.dispatcher = dispatcher
+
+        val mock = RetrofitFactory.mock(mockWebServer)
+        val response = mock.create(ApiService::class.java).getOverallEntries()
+        assertTrue(response.isSuccessful)
+        val body = response.body()
+        assertTrue(body is HatenaRss)
+    }
+}
