9d4ff82e01dc9d1d133212e76c289d1584738af8
Takuma Homma <takuma.honma@pepabo.com>
2021-03-13 19:02:38 +0900

    Fetch category list when Activity#onCreate

--- app/src/main/java/com/example/rssreader/ui/activity/MainActivity.kt
+++ app/src/main/java/com/example/rssreader/ui/activity/MainActivity.kt
@@ -1,18 +1,45 @@
 package com.example.rssreader.ui.activity
 
 import android.os.Bundle
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.Observer
 import com.example.rssreader.R
 import com.example.rssreader.databinding.ActivityMainBinding
+import com.example.rssreader.model.api.repository.FeedCategoryListRepository
+import com.example.rssreader.ui.viewmodel.MainViewModel
+import com.example.rssreader.ui.viewmodel.factory.MainViewModelFactory
 import com.example.rssreader.ui.widget.adapter.FeedPagerAdapter
+import com.google.android.material.tabs.TabLayoutMediator
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val binding = DataBindingUtil.setContentView<ActivityMainBinding>(
             this,
             R.layout.activity_main
         )
+        binding.lifecycleOwner = this
+        val viewPager = binding.activityMainViewpager
+
+        val viewModel: MainViewModel by viewModels {
+            MainViewModelFactory(FeedCategoryListRepository())
+        }
+
+        viewModel.categoryLiveData.observe(this, Observer {
+            if (it.categoryList.isNotEmpty()) {
+                val pagerAdapter = FeedPagerAdapter(
+                    this,
+                    it.categoryList.map { category -> category.path }
+                )
+                viewPager.adapter = pagerAdapter
+                TabLayoutMediator(binding.activityMainTabLayout, viewPager) { tab, position ->
+                    tab.text = it.categoryList[position].localizedName
+                }.attach()
+            }
+        })
+        viewModel.fetchCategoryList()
     }
 }
